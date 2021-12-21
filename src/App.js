0f6096452bc469658ccbbde4adf83feecd2db011
yashmanikonda <90308705+yashmanikonda@users.noreply.github.com>
2021-12-21 19:02:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useRef, useState } from 'react';
 import './App.css';
 
-import firebase from "firebase/compat/app";
-import 'firebase/compat/firestone';
-import 'firebase/compat/auth';
-import  'firebase/compat/analytics';
+import firebase from "firebase/app";
+import 'firebase/firestone';
+import 'firebase/auth';
+import  'firebase/analytics';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
