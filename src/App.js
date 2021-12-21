78afcea9013c8567130ce65bdc2e288d10aeda93
yashmanikonda <90308705+yashmanikonda@users.noreply.github.com>
2021-12-21 11:08:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestone';
+import 'firebase/auth';
+
+import {useAuthState} from 'react-firebase-hooks/auth';
+import {useCollectionData} from 'react-firebase-hooks/firestore';
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyC6gQS_CzG4Q8u1-W11S56ngIWxKrWMWFo",
+  authDomain: "projectchat-d0f2e.firebaseapp.com",
+  projectId: "projectchat-d0f2e",
+  storageBucket: "projectchat-d0f2e.appspot.com",
+  messagingSenderId: "362408400699",
+  appId: "1:362408400699:web:0d1d6e01cabae5aacb091e"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+function App() {
+
+  const[user] = useAuthState(auth);
+
+  return (
+    <div className="App">
+      <header>
+        
+      </header>
+
+      <section>
+        {user ? <ChatRoom /> : <SignIn/>}
+      </section>
+    </div>
+  );
+}
+
+function SignIn(){
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return(
+    <button onClick={signInWithGoogle}>Sign in with Google </button>
+  )
+
+
+}
+
+function ChatRoom() {
+  return auth.currentUser && (
+    <button onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+function ChatRoom(){
+  const dummy = useRef()
+
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+
+  const[messages] = useCollectionData(query, {idField: 'id'});
+
+  const [formValue, setFormValue] = useState('');
+
+  const sendMessage = async(e) => {
+
+    e.preventDefault();
+
+    const {uid,photoURL} = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+
+    });
+
+    setFormValue('');
+
+    dummy.current.scrollIntoView({ behavior: 'smooth'})
+  }
+
+  return (
+    <>
+      <main>
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+        <div ref={dummy}></div>
+      </main>
+
+      <form onSubmit={sendMessage}>
+        <input value={formValue} onChange={(e) => setFormValue(e.target.value)} />
+
+        <button type="submit"> </button>
+
+      </form>
+    
+    
+    </>
+  )
+
+  function ChatMessage(props) {
+    const{text, uid, photoURL} = props.message;
+
+    const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received' ;
+    return(
+      <div className={'message ${messageClass}'}>
+        <img src={photoURL}/>
+        <p>{text}</p>
+      </div>
+    )
+  }
+
+}
+
+export default App;
